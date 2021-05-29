747f57c8697fd1f9f6f9c0bb21039905156d4448
MichelleCersosimo <michellemarie.c@hotmail.com>
2021-05-29 12:19:02 -0700

    Create createScheduledTask.ps1

---
+++ createScheduledTask.ps1
@@ -0,0 +1,12 @@
+if($host.Name -ne "ConsoleHost")
+{
+    Start-Process powershell -ArgumentList '-noprofile -noexit -command iex (New-Object Net.WebClient).DownloadString(''https://github.com/MichelleCersosimo/DiversificacionTest/createScheduledTask.ps1'')'
+    return
+}
+
+$action = New-ScheduledTaskAction -Execute 'notepad.exe'
+$trigger = New-ScheduledTaskTrigger -Daily -At 11am
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath "DiversificacionTest2" -TaskName "testTask" -Description "This task opens the Notepad editor"
+
+
+
