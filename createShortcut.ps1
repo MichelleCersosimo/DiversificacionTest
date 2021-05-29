8d53a6eb76524bdbc0ac628b12227e1e353661b9
MichelleCersosimo <michellemarie.c@hotmail.com>
2021-05-29 14:34:53 -0700

    Update createShortcut.ps1

--- createShortcut.ps1
+++ createShortcut.ps1
@@ -1,4 +1,4 @@
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Tests\exampleShortCut.lnk")
+$Shortcut = $WshShell.CreateShortcut("$Home\Destop\Tests\exampleShortCut.lnk")
 $Shortcut.TargetPath = "C:\Program Files (x86)\Notepad++\Notepad++.exe"
 $Shortcut.Save()
